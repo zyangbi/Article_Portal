a66acbc81566903c668a44cbc6ae70e3af7beb2e
zyangbi <62045232+zyangbi@users.noreply.github.com>
2022-10-11 14:50:40 +0800

    swagger2

--- imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/HelloControllerApi.java
+++ imooc-news-dev-service-api/src/main/java/com/imooc/api/controller/user/HelloControllerApi.java
@@ -1,9 +1,13 @@
 package com.imooc.api.controller.user;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Api(value = "HelloController", tags = {"xxx"})
 public interface HelloControllerApi {
     @GetMapping("/hello")
+    @ApiOperation(value = "hello method", notes = "hello method", httpMethod = "GET")
     public Object hello();
 
 }
